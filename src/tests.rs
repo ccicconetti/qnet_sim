aefd9daea3767880e29674c429c4a46414171c8b
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-10-08 16:36:20 +0200

    Add unit test for multi-hop ES

--- src/tests.rs
+++ src/tests.rs
@@ -96,13 +96,17 @@ mod tests {
         }
     }
 
-    fn make_config(seed: u64) -> Config {
+    fn make_config(duration: f64, seed: u64, num_repeaters: u32) -> Config {
         let user_config = UserConfig {
-            duration: 10.0,
+            duration,
             warmup_period: 0.0,
             series_ignore: std::collections::HashSet::new(),
             physical_topology: PhysicalTopology::ConfChainStatic(ConfChainStatic {
-                chain_params: crate::physical_topology::ChainParams::default(),
+                chain_params: crate::physical_topology::ChainParams {
+                    orbit_to_orbit_distance: 3000000.0,
+                    ground_to_orbit_distance: 1000000.0,
+                    num_repeaters,
+                },
                 sat_weight: default_sat_weight(),
                 ogs_weight: default_ogs_weight(),
                 fidelities: crate::physical_topology::StaticFidelities {
@@ -125,7 +129,7 @@ mod tests {
     #[ignore]
     #[test]
     fn test_config_to_json() {
-        let config = make_config(42);
+        let config = make_config(1.0, 42, 1);
         println!("{}", serde_json::to_string_pretty(&config).unwrap());
     }
 
@@ -135,7 +139,7 @@ mod tests {
 
         let mut sim = None;
         for seed in 0..100 {
-            let cand_sim = Simulation::new(make_config(seed), false)?;
+            let cand_sim = Simulation::new(make_config(10.0, seed, 1), false)?;
             if cand_sim.logical_path(0, 1).len() == 2 {
                 sim = Some(cand_sim);
                 break;
@@ -163,11 +167,60 @@ mod tests {
                 values.stats()
             );
         }
-        assert_eq!(100, series.get("epr-request-latency").unwrap().values.len());
+        assert_eq!(100, series.get("app-net-latency").unwrap().values.len());
         assert_eq!(100, series.get("ping-latency").unwrap().values.len());
         assert_eq!(200, series.get("fidelity").unwrap().values.len());
         check_interval("fidelity", &series, 0.92, 1.0);
         check_interval("gen-fidelity", &series, 0.95, 0.99);
+        check_interval("app-tries", &series, 1.0, 1.0);
+        check_interval("occupancy", &series, 0.0, 1.0);
+        check_interval("app-path-len", &series, 1.0, 2.0);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_sim_multiple_logical_hops() -> anyhow::Result<()> {
+        // env_logger::init();
+
+        let mut sim = None;
+        for seed in 0..100 {
+            let cand_sim = Simulation::new(make_config(120.0, seed, 3), false)?;
+            if cand_sim.logical_path(0, 1).len() == 3 {
+                sim = Some(cand_sim);
+                break;
+            } else {
+                println!("seed {seed} does not have a 3-hop path from 0 to 1: skip")
+            }
+        }
+        let mut sim = sim.unwrap();
+
+        let output = sim.run();
+
+        let scalar = output.scalar.values();
+        println!("scalar: {:?}", scalar);
+        assert_float_eq::assert_f64_near!(1.0, *scalar.get("logical_topology_found").unwrap());
+        assert!(*scalar.get("event_queue_len").unwrap() > 0.0);
+        assert!(*scalar.get("num_events").unwrap() > 0.0);
+        assert!(*scalar.get("bsm_prob").unwrap() > 0.9);
+        assert!(*scalar.get("bsm_tot").unwrap() > 100.0);
+
+        let series = output.series.series;
+        for (metric, values) in &series {
+            println!(
+                "series {} stats (min/avg/max/len) {:?}",
+                metric,
+                values.stats()
+            );
+        }
+        assert_eq!(100, series.get("app-net-latency").unwrap().values.len());
+        assert_eq!(100, series.get("ping-latency").unwrap().values.len());
+        assert_eq!(200, series.get("fidelity").unwrap().values.len());
+        check_interval("fidelity", &series, 0.8, 1.0);
+        check_interval("gen-fidelity", &series, 0.95, 0.99);
+        check_interval("app-tries", &series, 1.0, 50.0);
+        check_interval("occupancy", &series, 0.0, 1.0);
+        check_interval("app-path-len", &series, 1.0, 3.0);
 
         Ok(())
     }
