55d83847a749db0879cef9718bb8bc4afb9d2ff3
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-10-10 09:34:12 +0000

    Experiment 001: add visualization scripts

---
+++ experiments/001-chain/scalar.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+DATA_DIR = os.environ.get("DATA_DIR", "data")
+IMAGE_TYPE = os.environ.get("IMAGE_TYPE", "pdf")
+
+basename = os.path.basename(os.getcwd())
+
+pd.set_option("display.show_dimensions", False)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_colwidth", None)
+df = pd.read_csv(f"{DATA_DIR}/scalar.csv")
+
+metrics = {
+    "epr_register_final_len": "EPR register final length",
+    "execution_time": "Simulation time (s)",
+    "logical_topology_found": "Logical topology found",
+    "num_events": "Total number of events",
+    "bsm_prob": "BSM probability",
+    "event_queue_len": "Event queue length",
+    "bsm_tot": "ES operations",
+    "epr_frees": "EPR free operations",
+    "local_epr_misses": "Local EPR misses",
+}
+
+primary = "num_repeaters"
+primary_label = "Chain size"
+secondaries = {"memory_qubits": "Q", "num_pairs": "P"}
+
+hue = None
+if secondaries:
+    for secondary, label in secondaries.items():
+        df[secondary] = label + df[secondary].astype("str")
+    df["hue"] = df[secondaries.keys()].agg("-".join, axis=1)
+    hue = "hue"
+
+for metric, ylabel in metrics.items():
+    fig, ax = plt.subplots()
+    sns.boxplot(
+        df,
+        x=primary,
+        y=metric,
+        hue=hue,
+        ax=ax,
+    )
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(primary_label)
+    legend = ax.get_legend()
+    if legend:
+        legend.set_title(title="")
+    # ax.set_ylim(bottom=0.01, top=10)
+    # ax.set_yscale("log")
+    # plt.xticks(rotation=45)
+    fig.suptitle(f"")
+    plt.savefig(f"{basename}-scalar-{metric}.{IMAGE_TYPE}")
---
+++ experiments/001-chain/series.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+DATA_DIR = os.environ.get("DATA_DIR", "data")
+IMAGE_TYPE = os.environ.get("IMAGE_TYPE", "pdf")
+
+basename = os.path.basename(os.getcwd())
+
+pd.set_option("display.show_dimensions", False)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_colwidth", None)
+
+metrics = {
+    "app-net-latency": "End-to-end network latency (s)",
+    "app-path-len": "Path length (hops)",
+    "app-tries": "Number of retries",
+    "fidelity": "Fidelity",
+    "ping-latency": "Ping latency (s)",
+}
+
+ylog_metrics = {"app-net-latency", "app-tries", "ping-latency"}
+
+primary = "num_repeaters"
+primary_label = "Chain size"
+secondaries = {"memory_qubits": "Q", "num_pairs": "P"}
+
+aggregates = {"mean": "Average", "p95": "95th percentile"}
+
+for metric, ylabel in metrics.items():
+    df = pd.read_csv(f"{DATA_DIR}/{metric}-stats.csv")
+
+    hue = None
+    if secondaries:
+        for secondary, label in secondaries.items():
+            df[secondary] = label + df[secondary].astype("str")
+        df["hue"] = df[secondaries.keys()].agg("-".join, axis=1)
+        hue = "hue"
+
+    for aggregate, aggregate_type in aggregates.items():
+        fig, ax = plt.subplots()
+        sns.boxplot(
+            df,
+            x=primary,
+            y=aggregate,
+            hue=hue,
+            ax=ax,
+        )
+        ax.set_ylabel(f"{ylabel} - {aggregate_type}")
+        ax.set_xlabel(primary_label)
+        legend = ax.get_legend()
+        if legend:
+            legend.set_title(title="")
+        # ax.set_ylim(bottom=0.01, top=10)
+        if metric in ylog_metrics:
+            ax.set_yscale("log")
+        # plt.xticks(rotation=45)
+        fig.suptitle(f"")
+        plt.savefig(f"{basename}-series-{metric}-{aggregate}.{IMAGE_TYPE}")
